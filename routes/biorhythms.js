22705b81b595f1c6277f719a6a3acb9ceae44227
Sebastián González <sgonzalez@debianchile.cl>
2019-03-19 17:13:20 -0300

    First commit

---
+++ routes/biorhythms.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const numerology = require('numerologic');
+const dayjs = require('dayjs');
+
+const router = express.Router();
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  const date = dayjs(`${req.body.year}-${req.body.month}-${req.body.day}`)
+    .format('DD.MM.YYYY');
+  const bioRhythms = numerology(date).bioRhythms();
+
+  res.json(bioRhythms);
+});
+
+module.exports = router;
