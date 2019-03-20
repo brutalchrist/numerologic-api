b9df33b3533bd7f1649e8c38ab3391157a81c7aa
Sebastián González <sgonzalez@debianchile.cl>
2019-03-19 22:38:38 -0300

    fix request

--- routes/biorhythms.js
+++ routes/biorhythms.js
@@ -5,8 +5,8 @@ const dayjs = require('dayjs');
 const router = express.Router();
 
 router.post('/', function(req, res, next) {
-  const date = dayjs(req.body.text)
-    .format('DD.MM.YYYY');
+  const date = dayjs(`${req.body.year}-${req.body.month}-${req.body.day}`)
+  .format('DD.MM.YYYY');
   const bioRhythms = numerology(date).bioRhythms();
 
   res.json(bioRhythms);
